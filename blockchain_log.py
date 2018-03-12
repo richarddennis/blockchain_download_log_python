dcb74235bd6bfe55e5b4709bbc054fa92539ae06
Richard Dennis <richard@dragoninfosec.com>
2018-03-12 19:19:29 +0000

    Create blockchain_log.py

---
+++ blockchain_log.py
@@ -0,0 +1,19 @@
+
+####pip install httplib2 and git clone github.com/jgarzik/python-bitcoinrpc.git and python setup.py install 
+
+import httplib2
+    from bitcoinrpc.authproxy import AuthServiceProxy
+    access = AuthServiceProxy('#########')
+    try:
+            blockCount = access.getblockcount()
+    except Exception as e:
+            print "Problems connecting to bitcoin wallet:"
+    else:
+            try:
+                    response, trueBlockCount = httplib2.Http().request("http://blockexplorer.com/q/getblockcount/")
+            except Exception as e:
+                    print "Unable to get true blockcount from blockexplorer:"+str(e)
+            else:
+                    if (int(trueBlockCount) - 5) > blockCount :
+                            print "blockchain not up to date: true block count is: "+str(trueBlockCount)+", while bitcoind is at: "+str(blockCount)
+                            
